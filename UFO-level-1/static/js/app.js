397c8fde8ed8aa702adcc9662dc2e9f5460a0794
Ruben <45341838+RubenZV@users.noreply.github.com>
2020-06-06 15:58:34 -0500

    Final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,85 +1,123 @@
-// from data.js
-var tableData = data;
+// Level 1:
+// Donde se va a almacenar el texto a desplegar
+var tbody = d3.select("#target-table-display");
+// Para el botòn de submit
+var submitButton = d3.select("#filter-btn-date");
+var inputField = d3.select("#datetime");
 
-// YOUR CODE HERE!
-// select tbody 
-tbody = d3.select("tbody")
-console.log("hello")
+var citysubmitButton = d3.select("#filter-btn-city");
+var cityinputField = d3.select("#cityname");
 
-// loop through table using pbject entries
-function displayData(something){ 
-    tbody.text("")
-    something.forEach(function(et_sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(et_sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
+var statesubmitButton = d3.select("#filter-btn-state");
+var stateinputField = d3.select("#statename");
 
-displayData(tableData)
+var countrysubmitButton = d3.select("#filter-btn-country");
+var countryinputField = d3.select("#countryname");
 
-console.log("hello2")
+var shapesubmitButton = d3.select("#filter-btn-shape");
+var shapeinputField = d3.select("#shapename");
 
 
-// Select the submit button
-var submit = d3.select("#submit");
-
-submit.on("click", function() {
-    console.log("hello3")
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var dateInput = d3.select("#datetime");
-  var cityInput = d3.select("#city");
-  var stateInput = d3.select("#state");
-  var countryInput = d3.select("#country");
-  var shapeInput = d3.select("#shape");
-
-  // Get the value property of the input element
-  console.log(dateInput.property("value"));
-  console.log(cityInput.property("value"));
-  console.log(stateInput.property("value"));
-  console.log(countryInput.property("value"));
-  console.log(shapeInput.property("value"));
-
-  //create a variable which filters the table if a user enters only some information in so that it will still work
-
- var filtered = tableData.filter(et_sighting =>{
-  return (et_sighting.datetime===dateInput.property("value") || !dateInput.property("value") ) && 
-            (et_sighting.city===cityInput.property("value") || !cityInput.property("value")) &&
-            (et_sighting.state===stateInput.property("value") || !stateInput.property("value")) &&
-            (et_sighting.country===countryInput.property("value") || !countryInput.property("value")) &&
-            (et_sighting.shape===shapeInput.property("value") || !shapeInput.property("value"))
- })
-
- //run the filtered entries through the displayData function to update the table
- displayData(filtered);
-
-
+// Creación de la tabla
+data.forEach(obj => {
+    var tRow = tbody.append("tr");
+    Object.entries(obj).forEach(([key,value]) => {
+        var tData = tRow.append("td");
+        tData.text(value);
+    });
 });
 
-var filterInputs = d3.selectAll('.form-control');
-
-// Clears input fields and input object
-function clearEntries() {
-    filters = {};
-
-    // Sets every input field to empty
-    filterInputs._groups[0].forEach(entry => {
-        if (entry.value != 0) {
-            d3.select('#' + entry.id).node().value = "";
-        }
-    });
-};
-
-var clearButton = d3.select("#clear");
-// Clear button on click clears fields
-clearButton.on('click', function () {
-
-    // Keeps page from refreshing completely, only want the table to refresh
+// Funcón del botón submit
+submitButton.on("click", function() {
+    tbody.html("");
     d3.event.preventDefault();
-    // Clears input fields
-    clearEntries()
+    var inputField = d3.select("#datetime");
+    var inputElement = inputField.property("value");
+    console.log(inputElement);
+    var inputTypeArray = data.filter(one => one.datetime === inputElement);
+    console.log(inputTypeArray)
+
+    inputTypeArray.forEach((selection) => {
+        var row = tbody.append("tr");
+        Object.entries(selection).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });      
+});
+
+// Level 2:
+// Realiza las búsquedas
+citysubmitButton.on("click", function() {
+    // clears the data of the current table        
+    tbody.html("");
+    d3.event.preventDefault();
+    var cityinputElement = cityinputField.property("value");
+    console.log(cityinputElement);
+    var cityinputTypeArray = data.filter(two => two.city === cityinputElement); 
+    console.log(cityinputTypeArray)
+
+    cityinputTypeArray.forEach((selects) => {
+        var Crow = tbody.append("tr");
+        Object.entries(selects).forEach(([key,value]) => {
+            var Ccell = Crow.append("td");
+            Ccell.text(value);
+        });
+    });      
+});
+
+
+statesubmitButton.on("click", function() {
+    // limpia la tabla       
+    tbody.html("");
+    d3.event.preventDefault();
+    
+    var stateinputElement = stateinputField.property("value");
+    console.log(stateinputElement);
+    var stateinputTypeArray = data.filter(three => three.state === stateinputElement);
+    console.log(stateinputTypeArray)
+
+    stateinputTypeArray.forEach((selectss) => {
+        var Srow = tbody.append("tr");
+        Object.entries(selectss).forEach(([key,value]) => {
+            var Scell = Srow.append("td");
+            Scell.text(value);
+        });
+    });      
+});
+
+
+countrysubmitButton.on("click", function() {    
+    tbody.html("");
+    d3.event.preventDefault();
+    
+    var countryinputElement = countryinputField.property("value");
+    console.log(countryinputElement);
+    var countryinputTypeArray = data.filter(four => four.country === countryinputElement);
+
+    countryinputTypeArray.forEach((selectsc) => {
+        var Corow = tbody.append("tr");
+        Object.entries(selectsc).forEach(([key,value]) => {
+            var Cocell = Corow.append("td");
+            Cocell.text(value);
+        });
+    });      
+});
+
+shapesubmitButton.on("click", function() {    
+    tbody.html("");
+    d3.event.preventDefault();
+    
+    var shapeinputElement = shapeinputField.property("value");
+    console.log(shapeinputElement);
+    var shapeinputTypeArray = data.filter(five => five.shape === shapeinputElement);
+    console.log(shapeinputTypeArray)
+
+    shapeinputTypeArray.forEach((selectsh) => {
+        var Shrow = tbody.append("tr");
+        Object.entries(selectsh).forEach(([key,value]) => {
+            var Shcell = Shrow.append("td");
+            Shcell.text(value);
+        });
+    });      
 });
